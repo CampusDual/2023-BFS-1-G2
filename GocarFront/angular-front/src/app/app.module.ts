44a8eb6352c2cb15cafd431c074adf5aff3b0a8e
daniMallo <101351451+Dmlsito@users.noreply.github.com>
2023-05-16 14:49:26 +0200

    Estructura de formulario hecha

--- GocarFront/angular-front/src/app/app.module.ts
+++ GocarFront/angular-front/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { FormuserComponent } from './formuser/formuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormuserComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +14,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
