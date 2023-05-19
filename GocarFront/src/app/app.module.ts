5be7f00987b3061acbdeb063aee19981b36781cf
Alexin-88 <alex_cabarcos@hotmail.com>
2023-05-19 09:16:14 +0200

    Creación header component

--- GocarFront/src/app/app.module.ts
+++ GocarFront/src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormuserComponent } from './formuser/formuser.component';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormuserComponent
+    FormuserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatToolbarModule
   
   ],
   providers: [],
