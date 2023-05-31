7fc56b765bf00927037a23257004c1add4637706
daniMallo <101351451+Dmlsito@users.noreply.github.com>
2023-05-31 10:31:13 +0200

    Cambio del comentario

--- GocarFrontOntimize/src/app/main/users/users-register/users-register.component.ts
+++ GocarFrontOntimize/src/app/main/users/users-register/users-register.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CONFIG } from 'src/app/app.config';
 @Component({
   selector: 'app-users-register',
   templateUrl: './users-register.component.html',
   styleUrls: ['./users-register.component.css']
 })
 export class UsersRegisterComponent implements OnInit {
-
+// NO HAGAIS CASO //
   constructor() { }
 
   ngOnInit() {
