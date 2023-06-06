fc870b3692051684f32c1a3f307d3e6ee747dd15
Samu <124720302+samucorre@users.noreply.github.com>
2023-06-06 12:52:07 +0200

    Update app.menu.config.ts

--- GoCarFrontOntimize/src/app/shared/app.menu.config.ts
+++ GoCarFrontOntimize/src/app/shared/app.menu.config.ts
@@ -4,8 +4,8 @@ import {
 } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'users', name: 'USERS', icon: 'people', route: '/main/users' },
-  {id: 'cars', name: 'CARS', icon: 'directions_car', route: '/main/cars'},
-  { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
+  { id: 'home', name: 'Home', icon: 'home', route: '/main/home' },
+  { id: 'users', name: 'Users', icon: 'people', route: '/main/users' },
+  {id: 'cars', name: 'Cars', icon: 'directions_car', route: '/main/cars'},
+  { id: 'logout', name: 'Logout', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];
