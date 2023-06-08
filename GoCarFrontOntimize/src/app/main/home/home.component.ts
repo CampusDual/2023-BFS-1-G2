5a44be4f5b4393d0816499d7187dfd3c3ecb4a06
daniMallo <101351451+Dmlsito@users.noreply.github.com>
2023-06-08 10:41:42 +0200

    Metodo para recuperar todos los coches que ya se ejecutara en el onInit del componente home

--- GoCarFrontOntimize/src/app/main/home/home.component.ts
+++ GoCarFrontOntimize/src/app/main/home/home.component.ts
@@ -14,8 +14,37 @@ export class HomeComponent implements OnInit {
   ) {
   }
 
-  ngOnInit() {
-    // nothing to do
+  username: string = 'demo'
+  password: string = 'demouser'
+
+  auth: string = "Basic " + btoa(`${this.username}:${this.password}`)
+
+  carsList: Array<Object>
+
+  protected body = {
+    filter: {},
+    columns: ["brand","model","status","il","password"]}
+
+  async ngOnInit() {
+
+    try{
+
+      const { data } = await fetch('http://localhost:33333/cars/car/search', {
+        method: 'POST', 
+        body: JSON.stringify(this.body),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          "Authorization": this.auth
+      }
+      }).then(res => res.json())
+
+      this.carsList = data
+      console.log(this.carsList)
+
+    }catch(e){
+
+      console.log(e.message)
+    } 
   }
 
   navigate() {
--- GocarFrontOntimize/src/app/main/cars/cars-new/cars-new.component.ts
+++ GocarFrontOntimize/src/app/main/cars/cars-new/cars-new.component.ts
@@ -13,66 +13,42 @@ export class CarsNewComponent implements OnInit {
 
   protected carService : OntimizeService;
 
-  username: string = 'demo'
-  password: string = 'demouser'
-
-  auth: string = "Basic " + btoa(`${this.username}:${this.password}`)
-
-  protected body = {
-    filter: {},
-    columns: ["brand","model","status","il","password"]}
-  
   @ViewChild('form', { static: false }) form: OFormComponent;
   
   constructor(public injector : Injector, private dialogRef: MatDialogRef<CarsNewComponent>,) {  
-      
-      this.carService = this.injector.get(OntimizeService);
-       }
 
-  ngOnInit() {}
+    this.carService = this.injector.get(OntimizeService);
 
-  public async send(){
+  }
+
+    ngOnInit() {}
+
+    public async send(){
 
       this.form.insert();
+    
     }
 
   
     public configureCarService(){
+
       const conf = this.carService.getDefaultServiceConfiguration('cars');
       this.carService.configureService(conf);
+
     }
 
     public forceInsertMode(event: any) {
+
       if (event != OFormComponent.Mode().INSERT) {
         this.form.setInsertMode();
-    
       }
+      
     }
     
    public closeDialog(event: any) {
      this.dialogRef.close();
   }
   
-  public async getAllCars(body: Object, auth: string) {
-
-    try{
-
-      const data = await fetch('http://localhost:33333/cars/car/search', {
-        method: 'POST', 
-        body: JSON.stringify(body),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          "Authorization": auth
-      }
-      }).then(res => res.json())
-
-      return data
-
-    }catch(e){
-
-      console.log(e.message)
-    } 
-  }
   
 
 }
