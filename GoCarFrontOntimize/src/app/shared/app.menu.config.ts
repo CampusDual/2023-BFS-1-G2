45bacda34509397eaf7e7f353ad114abcbf9ee39
Alexin-88 <alex_cabarcos@hotmail.com>
2023-06-14 13:00:07 +0200

    Logo y modificar entity en home

--- GoCarFrontOntimize/src/app/shared/app.menu.config.ts
+++ GoCarFrontOntimize/src/app/shared/app.menu.config.ts
@@ -4,7 +4,7 @@ import {
 } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
-  { id: 'home', name: 'cars renting', icon: 'home', route: '/main/home' },
+  { id: 'home', name: 'cars renting', icon: 'car_rental', route: '/main/home' },
   // { id: 'users', name: 'users', icon: 'people', route: '/main/users' },
   {id: 'cars', name: 'My Cars', icon: 'directions_car', route: '/main/cars'},
   { id: 'logout', name: 'logout', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
