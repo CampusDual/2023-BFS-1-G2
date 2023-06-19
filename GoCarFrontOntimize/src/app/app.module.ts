77618597fd33fd5cbfdb5014f756ba0c27d04db1
Alexin-88 <alex_cabarcos@hotmail.com>
2023-06-19 09:16:36 +0200

    Borrado import

--- GoCarFrontOntimize/src/app/app.module.ts
+++ GoCarFrontOntimize/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CONFIG } from './app.config';
-import { CustomMessageService, CustomMessageServiceUser } from './main/util/CustomMessageUser';
+import { CustomMessageServiceUser } from './main/util/CustomMessageUser';
 import { CustomMessageServiceCar } from './main/util/CustomMessageCars';
 
 
