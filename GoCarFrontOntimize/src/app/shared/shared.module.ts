9dcd6204615a349fbc26e1f0b7ec50b63dcea131
Samu <124720302+samucorre@users.noreply.github.com>
2023-07-04 18:17:29 +0200

    correccion de errores

--- GoCarFrontOntimize/src/app/shared/shared.module.ts
+++ GoCarFrontOntimize/src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
 import { OChartModule } from 'ontimize-web-ngx-charts';
+import { OMapModule } from "ontimize-web-ngx-map";
 
 @NgModule({
   imports: [
     OntimizeWebModule,
     OChartModule,
+    OMapModule
    
   ],
   declarations: [
   ],
   exports: [
     CommonModule,
+    OMapModule
 
   ]
 })
