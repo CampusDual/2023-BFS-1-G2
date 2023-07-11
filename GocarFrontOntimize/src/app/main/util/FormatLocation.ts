797c949c39184f90637a1101aec4188f78e8fd31
Samu <124720302+samucorre@users.noreply.github.com>
2023-07-11 10:01:29 +0200

    Update FormatLocation.ts
    Solución a formato array localizacion

--- GocarFrontOntimize/src/app/main/util/FormatLocation.ts
+++ GocarFrontOntimize/src/app/main/util/FormatLocation.ts
@@ -1,12 +1,15 @@
 export class FormatLocation {
   public format(infoLocation: string): any[] {
     let resFinal = infoLocation.split(",");
-
+  
     resFinal = resFinal.slice(2);
-    resFinal.toString().split(" ");
-    let ciudad = resFinal[0].split(" ")[2];
+    let infoCiudad = resFinal[0];
+    const city= infoCiudad.slice(7, infoCiudad.length);    
+    resFinal.toString().split(" ");    
     resFinal.shift();
-    resFinal.unshift(ciudad);
+    resFinal.unshift(city);
+    // console.log(resFinal)
     return resFinal;
+    
   }
 }
